85e1c047581a2cfd321659c611cedd0303ccc32c
Lotta Melcher <lotta.melcher@code.berlin>
2020-03-18 14:44:46 +0100

    Add: Test for function move_short_to_fullname

--- tests/test_app.r
+++ tests/test_app.r
@@ -10,7 +10,6 @@ context("Test Game Functions")
 source("../R/game.r")
 
 # test create_params(players, x-cost, y-cost, imi-cost)
-
 test_that("create_params: stop if players aren't character vector", {
   expect_error(create_params(3, 15, 45, 20))
 })
@@ -22,4 +21,11 @@ test_that("create_params: player starts always on the left", {
 
 test_that("create_params: a list is returned", {
   expect_identical(typeof(create_params(c("Computer", "You"), 15, 45, 20)), "list")
+})
+
+# test move_short_to_fullname(shortname)
+test_that("move_short_to_fullname: shortname is converted or NULL", {
+  expect_identical(move_short_to_fullname("c"), "Check (do nothing)")
+  expect_identical(move_short_to_fullname("il"), "Imitate & adjust marketing left")
+  expect_null(move_short_to_fullname("NEIN"))
 })
